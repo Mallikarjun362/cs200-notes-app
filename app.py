1def54780e933238340c9e454670933554b23dd3
Soumen Jana <jsoumen444@gmail.com>
2023-10-16 00:29:55 +0530

    authentication part added

--- app.py
+++ app.py
@@ -8,6 +8,30 @@ app = Flask(__name__)
 def home_page():
     return render_template("home_page.html")
 
+# --------------------------------------------------- USER -------------------------------------------
+# API - USER REGISTER
+@app.route("/api/user/register", methods=["POST"])
+def user_create():
+    data = request.get_json()
+    if not is_user_exists(data["username"]):
+        new_user = User(data["username"], hash_password(data["password"]))
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"msg": "SUCCESS"})
+    else:
+        return jsonify({"msg": "USER_EXISTS"})
+
+
+# API - USER LOGIN
+@app.route("/api/user/login", methods=["POST"])
+def user_login():
+    data = request.get_json()
+    if authenticate_user(data["username"], data["password"]):
+        return jsonify({"msg": "SUCCESS"})
+    else:
+        return jsonify({"msg": "USER_INVALID"})
+
+
 
 # --------------------------------------------------- NOTES -------------------------------------------
 # API - NOTES CREATE
