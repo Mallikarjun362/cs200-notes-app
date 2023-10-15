eca95eb22e75ba6bcac6dcd86e07b7f6ff536c00
Mallikarjun362 <pmallikarjun307@gmail.com>
2023-10-16 00:04:41 +0530

    Added Outline JS Code

---
+++ static/js/code.js
@@ -0,0 +1,17 @@
+// GLOBAL VARIABLES
+
+function user_login() { }
+function user_register() { }
+function user_logout() { }
+
+function notes_get_all() { }
+function notes_create() { }
+function notes_delete(id) { }
+
+
+// USER COMPONENT
+function render_in() { }
+function render_out() { }
+
+
+window.onload = function () { };
