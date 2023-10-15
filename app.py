4772ee20873c9c717afb9aa92895ca123508db45
Mallikarjun362 <pmallikarjun307@gmail.com>
2023-10-15 23:08:16 +0530

    added api routes for Notes related operations

--- app.py
+++ app.py
@@ -9,5 +9,24 @@ def home_page():
     return render_template("home_page.html")
 
 
+# --------------------------------------------------- NOTES -------------------------------------------
+# API - NOTES CREATE
+@app.route("/api/notes/create", methods=["POST"])
+def notes_create():
+    return "HELLO"
+
+
+# API - NOTES READ
+@app.route("/api/notes/read", methods=["POST"])
+def notes_read():
+    return "HELLO"
+
+
+# API - NOTES DELETE
+@app.route("/api/notes/delete", methods=["POST"])
+def notes_delete():
+    return "HELLO"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
