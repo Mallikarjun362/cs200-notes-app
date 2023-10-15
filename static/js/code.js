816eea3a0ec9818f20fbe131f2058791bca3421f
Mallikarjun362 <pmallikarjun307@gmail.com>
2023-10-16 00:07:14 +0530

    Added User Component JS Functions

--- static/js/code.js
+++ static/js/code.js
@@ -8,9 +8,62 @@ const div_display_user = document.getElementById('display_username');
 const in_username = document.getElementById('username');
 const in_password = document.getElementById('password');
 
-function user_login() { }
-function user_register() { }
-function user_logout() { }
+// DONE
+function user_login() {
+    const username = in_username.value;
+    const password = in_password.value;
+    const data = { username, password }
+    axios.post('/api/user/login', data)
+        .then(function (response) {
+            const msg = response.data.msg;
+            if (msg === "SUCCESS") {
+                localStorage.setItem("username", username);
+                localStorage.setItem("password", password);
+                USERNAME = username;
+                PASSWORD = password;
+                render_in();
+                notes_get_all();
+            } else if (msg = "USER_INVALID") {
+                document.getElementById("error-msg").innerText = "INVALID CREDENTIALS";
+            }
+        })
+        .catch(function (error) {
+            console.error('Error');
+            document.getElementById("error-msg").innerText = "INVALID CREDENTIALS";
+        });
+}
+
+// DONE
+function user_register() {
+    const username = in_username.value;
+    const password = in_password.value;
+    const data = { username, password }
+    axios.post('/api/user/register', data)
+        .then(function (response) {
+            const msg = response.data.msg;
+            if (msg === "SUCCESS") {
+                localStorage.setItem("username", username);
+                localStorage.setItem("password", password);
+                USERNAME = username;
+                PASSWORD = password;
+                render_in();
+            } else if (msg === "USER_EXISTS") {
+                document.getElementById("error-msg").innerText = "USER ALREADY EXISTS";
+            }
+        })
+        .catch(function (error) {
+            console.error('Error');
+        });
+}
+// DONE
+function user_logout() {
+    localStorage.removeItem("username")
+    localStorage.removeItem("password")
+    USERNAME = null;
+    PASSWORD = null;
+    // render_out();
+    location.reload();
+}
 
 function notes_get_all() { }
 function notes_create() { }
@@ -18,8 +71,29 @@ function notes_delete(id) { }
 
 
 // USER COMPONENT
-function render_in() { }
-function render_out() { }
+function render_in() {
+    console.log("USER IN");
+    div_display_user.innerText = localStorage.getItem("username");
+    div_user_in.style.display = 'flex';
+    div_user_out.style.display = 'none';
+
+}
+
+function render_out() {
+    console.log("USER OUT");
+    div_user_in.style.display = 'none';
+    div_user_out.style.display = 'flex';
+}
 
 
-window.onload = function () { };
+window.onload = function () {
+    if (localStorage.getItem('username') === null) {
+        render_out();
+    } else {
+        render_in();
+        USERNAME = in_username.value;
+        PASSWORD = in_password.value;
+        notes_get_all();
+    }
+
+};
