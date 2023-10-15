a6ad000ed5c99df87e1445cf4590a5cc4c37e747
Soumen Jana <jsoumen444@gmail.com>
2023-10-15 22:02:25 +0530

    added padding fun for the key

--- crypto.py
+++ crypto.py
@@ -8,3 +8,16 @@ def pad(text):
     padding = block_size - len(text) % block_size
     return text + bytes([padding] * padding)
 
+#padding the key to AES 128 bit key
+def pad_string_to_128_bytes(input_string):
+    desired_length = 16
+
+    input_bytes = input_string.encode("utf-8")
+    if len(input_bytes) >= desired_length:
+        result_bytes = input_bytes[:desired_length]
+    else:
+        padding_length = desired_length - len(input_bytes)
+        padding = b"\x00" * padding_length
+        result_bytes = input_bytes + padding
+
+    return result_bytes
