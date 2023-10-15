642edf3b681a4e78c5beea8a8a8c59620b7f0c96
Mallikarjun362 <pmallikarjun307@gmail.com>
2023-10-16 00:05:27 +0530

    Added Global Variables to JS file

--- static/js/code.js
+++ static/js/code.js
@@ -1,4 +1,12 @@
 // GLOBAL VARIABLES
+let USERNAME = ""
+let PASSWORD = ""
+let LIST_NOTES_OBJ = [];
+const div_user_in = document.getElementById('user_in');
+const div_user_out = document.getElementById('user_out');
+const div_display_user = document.getElementById('display_username');
+const in_username = document.getElementById('username');
+const in_password = document.getElementById('password');
 
 function user_login() { }
 function user_register() { }
